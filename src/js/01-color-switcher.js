17a52a641038af347b6b5a4052a056c47721633e
Oleksandr Havryliuk <deolis.ua@gmail.com>
2022-02-04 12:31:07 +0200

    task-1: done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+startBtn.addEventListener('click', changeBgColor);
+stopBtn.addEventListener('click', stopChangingBgColor);
+stopBtn.disabled = true;
+
+// function changeBgColor() {
+//   timerId = setInterval(function randomBgColor() {
+//     document.body.style.backgroundColor = getRandomHexColor();
+//     return randomBgColor;
+//   }(), 1000);
+//   startBtn.disabled = true;
+//   stopBtn.disabled = false;
+// }
+
+function changeBgColor() {
+  function randomBgColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }
+  randomBgColor();
+  timerId = setInterval(randomBgColor, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+}
+
+function stopChangingBgColor() {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}

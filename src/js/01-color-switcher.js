c0cba835d930de94d07db362f82f1c144de756f5
Oleksandr Havryliuk <deolis.ua@gmail.com>
2022-02-06 18:22:09 +0200

    task-1: style

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -9,15 +9,6 @@ startBtn.addEventListener('click', changeBgColor);
 stopBtn.addEventListener('click', stopChangingBgColor);
 stopBtn.disabled = true;
 
-// function changeBgColor() {
-//   timerId = setInterval(function randomBgColor() {
-//     document.body.style.backgroundColor = getRandomHexColor();
-//     return randomBgColor;
-//   }(), 1000);
-//   startBtn.disabled = true;
-//   stopBtn.disabled = false;
-// }
-
 function changeBgColor() {
   function randomBgColor() {
     document.body.style.backgroundColor = getRandomHexColor();
@@ -33,3 +24,25 @@ function stopChangingBgColor() {
   startBtn.disabled = false;
   stopBtn.disabled = true;
 }
+
+//---------------STYLES---------------\\
+startBtn.style.display = 'block';
+startBtn.style.marginLeft = 'auto';
+startBtn.style.marginRight = 'auto';
+startBtn.style.marginBottom = '20px';
+startBtn.style.padding = '5px';
+startBtn.style.width = '300px';
+startBtn.style.height = '50px';
+startBtn.style.fontSize = '24px';
+startBtn.style.fontWeight = '700';
+startBtn.style.borderWidth = '5px';
+
+stopBtn.style.display = 'block';
+stopBtn.style.marginLeft = 'auto';
+stopBtn.style.marginRight = 'auto';
+stopBtn.style.padding = '5px';
+stopBtn.style.width = '300px';
+stopBtn.style.height = '50px';
+stopBtn.style.fontSize = '24px';
+stopBtn.style.fontWeight = '700';
+stopBtn.style.borderWidth = '5px';
